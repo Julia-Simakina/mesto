58dc93c0adf51240dc3eb00dd77d9c1dee89063d
Julia Simakina <simakina.js@yandex.ru>
2023-02-20 22:26:30 +0300

    2 итерация

--- scripts/index.js
+++ scripts/index.js
@@ -1,25 +1,27 @@
 let popup = document.querySelector('.popup');
 
 let editPopup = document.querySelector('.profile__edit-button');
-let closePopup = document.querySelector('.popup__close');
+let closeButtonPopup = document.querySelector('.popup__close');
 
 let profileTitle = document.querySelector('.profile__title');
 let profileSubtitle = document.querySelector('.profile__subtitle');
-
-function changePopup() {
-  popup.classList.toggle('popup_opened');
-  nameInput.value = profileTitle.textContent;
-  jobInput.value = profileSubtitle.textContent;
-}
-
-editPopup.addEventListener('click', changePopup);
-closePopup.addEventListener('click', changePopup);
 
 // Находим форму в DOM
 let formElement = document.querySelector('.form');
 // Находим поля формы в DOM
 let nameInput = document.querySelector('.form__input_type_name');
 let jobInput = document.querySelector('.form__input_type_job');
+
+function openPopup() {
+  popup.classList.add('popup_opened');
+  nameInput.value = profileTitle.textContent;
+  jobInput.value = profileSubtitle.textContent;
+}
+
+function closePopup() {
+  popup.classList.remove('popup_opened');
+}
+
 // Обработчик «отправки» формы, хотя пока
 // она никуда отправляться не будет
 function handleFormSubmit(evt) {
@@ -29,9 +31,11 @@ function handleFormSubmit(evt) {
 
   profileTitle.textContent = nameInput.value;
   profileSubtitle.textContent = jobInput.value;
-  changePopup();
+  closePopup();
 }
 
+editPopup.addEventListener('click', openPopup);
+closeButtonPopup.addEventListener('click', closePopup);
 // Прикрепляем обработчик к форме:
 // он будет следить за событием “submit” - «отправка»
 formElement.addEventListener('submit', handleFormSubmit);
